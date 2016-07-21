af4e1a7903f2ddd23980b0a66f9facb3b3567155
Joe McIntyre <j_h_mcintyre@yahoo.com>
2016-07-21 16:33:11 -0500

    Update versions

--- src/client/main/components/AboutPage.jsx
+++ src/client/main/components/AboutPage.jsx
@@ -11,10 +11,10 @@ export default class AboutPage extends React.Component {
         <a href='https://github.com/fcc-joemcintyre/pollster' target='_blank'>Link</a>)</p>
         <p>Technologies used include:</p>
         <ul>
-          <li>Client: React (15.0.x), React-Redux and React-Router</li>
-          <li>Server: Node (6.1.x) using Express and Passport</li>
-          <li>Database: Mongo (3.0.x)</li>
-          <li>Languages: Javascript (ES2015), CSS (Sass)</li>
+          <li>Client: React (15.x), React-Redux and React-Router</li>
+          <li>Server: Node (6.x) using Express and Passport</li>
+          <li>Database: Mongo (3.x)</li>
+          <li>Languages: Javascript (ES2015), CSS (SCSS using Sass)</li>
         </ul>
         <p>Thanks to:</p>
         <ul>
