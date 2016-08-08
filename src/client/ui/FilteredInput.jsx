7d75a2bd27aa7d10fbf7cda8786dcd6c1a49bd9d
Joe McIntyre <j_h_mcintyre@yahoo.com>
2016-08-07 20:08:48 -0500

    FilteredInput to stateless function

--- src/client/ui/FilteredInput.jsx
+++ src/client/ui/FilteredInput.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-export default class FilteredInput extends React.Component {
-  render () {
-    return (
-      <input {...this.props}
-        onKeyPress={(e) => {
-          if (this.props.filter.test (e.key) === false) {
-            e.preventDefault ();
-          }
+const FilteredInput = (props) => {
+  return (
+    <input
+      {...props}
+      onKeyPress={(e) => {
+        if (this.props.filter.test (e.key) === false) {
+          e.preventDefault ();
         }
-      }/>
-    );
-  }
-}
+      }}
+    />
+  );
+};
 
 FilteredInput.PropTypes = {
-  filter: React.PropTypes.object.isRequired
-}
+  filter: React.PropTypes.object.isRequired,
+};
+
+export default FilteredInput;
