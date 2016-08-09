9fdf7803da4694cd326cb7e9857a29d93739d575
Joe McIntyre <j_h_mcintyre@yahoo.com>
2016-08-09 14:54:41 -0500

    update build for eslint compliance

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-'use strict';
-let gulp = require ('gulp');
-let gutil = require ('gulp-util');
-let cssmin = require ('gulp-cssmin');
-let sass = require ('gulp-sass');
-let buffer = require ('vinyl-buffer');
-let source = require ('vinyl-source-stream');
-let babelify = require ('babelify');
-let browserify = require ('browserify');
-let watchify = require ('watchify');
-let uglify = require ('gulp-uglify');
-let gzip = require ('gulp-gzip');
-let sourcemaps = require ('gulp-sourcemaps');
+/* eslint prefer-arrow-callback: off */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const gulp = require ('gulp');
+const gutil = require ('gulp-util');
+const cssmin = require ('gulp-cssmin');
+const sass = require ('gulp-sass');
+const buffer = require ('vinyl-buffer');
+const source = require ('vinyl-source-stream');
+const babelify = require ('babelify');
+const browserify = require ('browserify');
+const watchify = require ('watchify');
+const uglify = require ('gulp-uglify');
+const gzip = require ('gulp-gzip');
+const sourcemaps = require ('gulp-sourcemaps');
 
-let dependencies = [
+const dependencies = [
   'babel-polyfill',
   'react',
   'react-dom',
@@ -20,9 +21,9 @@ let dependencies = [
   'react-router',
   'redux',
   'redux-thunk',
-  'whatwg-fetch'
+  'whatwg-fetch',
 ];
-let stageDir = '../pollster-stage';
+const stageDir = '../pollster-stage';
 let base = 'dist';
 
 gulp.task ('default', ['html', 'images', 'server', 'styles', 'vendor',
@@ -32,7 +33,7 @@ gulp.task ('stage', ['set-stage', 'html', 'images', 'server', 'styles',
 
 // set the destination for staging output and copy stage root files
 gulp.task ('set-stage', function () {
-  base = stageDir + '/dist';
+  base = `${stageDir}/dist`;
   return gulp.src (['stage/*', 'stage/.*'])
     .pipe (gulp.dest (stageDir));
 });
@@ -49,13 +50,13 @@ gulp.task ('watch', function () {
 // copy index.html and favicon.ico
 gulp.task ('html', function () {
   return gulp.src (['src/client/index.html', 'src/client/favicon.ico'])
-    .pipe (gulp.dest (base + '/public'));
+    .pipe (gulp.dest (`${base}/public`));
 });
 
 // copy images
 gulp.task ('images', function () {
   return gulp.src ('src/client/images/*.*')
-    .pipe (gulp.dest (base + '/public/images'));
+    .pipe (gulp.dest (`${base}/public/images`));
 });
 
 // copy server
@@ -73,7 +74,7 @@ gulp.task ('vendor', function () {
     .pipe (buffer ())
     .pipe (uglify ({ mangle: false }))
     .pipe (gzip ({ append: true }))
-    .pipe (gulp.dest (base + '/public/js'));
+    .pipe (gulp.dest (`${base}/public/js`));
 });
 
 // compile stylesheets
@@ -81,32 +82,33 @@ gulp.task ('styles', function () {
   return gulp.src ('src/client/css/main.scss')
     .pipe (sass ().on ('error', sass.logError))
     .pipe (cssmin ())
-    .pipe (gulp.dest (base + '/public/css'));
+    .pipe (gulp.dest (`${base}/public/css`));
 });
 
 // compile and package application
 gulp.task ('browserify-watch', function () {
-  let bundler = watchify (browserify ({ entries: 'src/client/main/components/App.jsx', debug: true }, watchify.args));
+  const config = { entries: 'src/client/main/components/App.jsx', debug: true };
+  const bundler = watchify (browserify (config, watchify.args));
   bundler.external (dependencies);
   bundler.transform (babelify, { presets: ['es2015', 'react'] });
   bundler.on ('update', rebundle);
   return rebundle ();
 
   function rebundle () {
-    let start = Date.now ();
+    const start = Date.now ();
     return bundler.bundle ()
       .on ('error', function (err) {
         gutil.log (gutil.colors.red (err.toString ()));
       })
       .on ('end', function () {
-        gutil.log (gutil.colors.green ('Finished rebundling in', (Date.now () - start) + 'ms.'));
+        gutil.log (gutil.colors.green ('Finished rebundling in', Date.now () - start, 'ms.'));
       })
       .pipe (source ('bundle.js'))
       .pipe (buffer ())
       .pipe (gzip ({ append: true }))
       .pipe (sourcemaps.init ({ loadMaps: true }))
       .pipe (sourcemaps.write ('.'))
-      .pipe (gulp.dest (base + '/public/js/'));
+      .pipe (gulp.dest (`${base}/public/js/`));
   }
 });
 
@@ -120,30 +122,30 @@ gulp.task ('vendor-stage', function () {
     .pipe (buffer ())
     .pipe (uglify ({ mangle: false }))
     .pipe (gzip ({ append: true }))
-    .pipe (gulp.dest (base + '/public/js'));
+    .pipe (gulp.dest (`${base}/public/js`));
 });
 
 gulp.task ('browserify-stage', function () {
   process.env.NODE_ENV = 'production';
-  let bundler = browserify ({ entries: 'src/client/main/components/App.jsx', debug: true });
+  const bundler = browserify ({ entries: 'src/client/main/components/App.jsx', debug: true });
   bundler.external (dependencies);
   bundler.transform (babelify, { presets: ['es2015', 'react'] });
   bundler.on ('update', rebundle);
   return rebundle ();
 
   function rebundle () {
-    let start = Date.now ();
+    const start = Date.now ();
     return bundler.bundle ()
       .on ('error', function (err) {
         gutil.log (gutil.colors.red (err.toString ()));
       })
       .on ('end', function () {
-        gutil.log (gutil.colors.green ('Finished rebundling in', (Date.now () - start) + 'ms.'));
+        gutil.log (gutil.colors.green ('Finished rebundling in', Date.now () - start, 'ms.'));
       })
       .pipe (source ('bundle.js'))
       .pipe (buffer ())
       .pipe (uglify ({ mangle: false }))
       .pipe (gzip ({ append: true }))
-      .pipe (gulp.dest (base + '/public/js/'));
+      .pipe (gulp.dest (`${base}/public/js/`));
   }
 });
