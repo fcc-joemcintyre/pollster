b6e07bb8eb9cf539785ba3226edef649a1b65166
Joe McIntyre <j_h_mcintyre@yahoo.com>
2017-07-19 12:20:35 -0500

    Fix database name

--- test/db/test-general.js
+++ test/db/test-general.js
@@ -1,7 +1,7 @@
 const db = require ('../../dist/db');
 
 // mongo URI with port number not an active MongoDB instance
-const mongoUri = 'mongodb://localhost:27017/pinsterTest';
+const mongoUri = 'mongodb://localhost:27017/pollsterTest';
 
 describe ('init/close', function () {
   describe ('call init twice', function () {
