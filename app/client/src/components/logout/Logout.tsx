104e86ecc8d59b1832cd6b23e5eb072f65eb3431
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-05-12 12:41:48 -0500

    client: add return to home button to logout page

--- app/client/src/components/logout/Logout.tsx
+++ app/client/src/components/logout/Logout.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from 'react';
-import { Typography } from '@mui/material';
+import { useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { Box, Button, Typography } from '@mui/material';
 import { PageContent } from '../util';
 import { useLogout } from '../../data/useAuth';
 
 export const Logout = () => {
+  const navigate = useNavigate ();
   const logout = useLogout ();
 
   useEffect (() => logout.mutate (), []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onHome = useCallback (() => navigate ('/'), [navigate]);
 
   return (
     <PageContent>
@@ -19,9 +23,17 @@ export const Logout = () => {
           Logging out did not complete, please retry or close your browser.
         </Typography>
       ) : (
-        <Typography mt='30px' textAlign='center'>
-          Thank you for using Pollster, we hope to see you back again soon.
-        </Typography>
+        <Box mt='30px' textAlign='center'>
+          <Typography paragraph>
+            Thank you for using Pollster, we hope to see you back again soon.
+          </Typography>
+          <Button
+            type='button'
+            onClick={onHome}
+          >
+            Home
+          </Button>
+        </Box>
       )}
     </PageContent>
   );
