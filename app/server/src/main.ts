8ad5bd01eeb9ea9186ed769e275c4a9cc17e2f1d
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-09-13 14:47:10 -0500

    server: add database using log message

--- app/server/src/main.ts
+++ app/server/src/main.ts
@@ -15,5 +15,12 @@ function main (): void {
   const port = Number (process.env.PORT) || command.port;
   const uri = command.db === 'mongodb' ?
     process.env.MONGODB_URI || 'mongodb://localhost:27017/pollster' : '';
+  if (uri) {
+    if (process.env.MONGODB_URI) {
+      console.log ('INFO Using production database');
+    } else {
+      console.log ('INFO Using local database');
+    }
+  }
   startServer (port, uri);
 }
